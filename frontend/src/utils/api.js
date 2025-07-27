e951b737af6a55930a8e31a5cd165f9d0d083459
sridhar1030 <sridharpillai75@gmail.com>
2025-07-28 01:32:06 +0530

    changed api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000/api/v1/auth";
+const API_BASE_URL = import.meta.env.VITE_API;
 
 // Common API request function
 export const apiRequest = async (endpoint, options = {}) => {
