984153ba6ac492d2b4ac43d24a52b967e9bd1998
sridhar1030 <sridharpillai75@gmail.com>
2025-07-27 20:28:38 +0530

    FirstCommit

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express({
+	origin: process.env.CORS_ORIGIN,
+	credentials: true,
+});
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+import { userRouter } from "./routers/user.routes.js";
+
+app.use("/api/v1/auth", userRouter);
+
+export { app };
+    
